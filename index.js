cdcbb8ed861e197fa403a6155de50bbb9a2b5602
Viktoria <doduchenko29@gmail.com>
2021-06-09 17:07:34 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const buttonElem = Array.from(document.querySelectorAll('.pagination__page'));
 function handleClick(event) {
-  console.log(+Object.values(event.target.dataset));
+  console.log(Object.values(event.target.dataset)[0]);
 }
 buttonElem[0].addEventListener('click', handleClick);
 buttonElem[1].addEventListener('click', handleClick);
